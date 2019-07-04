870a4a15d3b640fb164b1552e18a65778a899bd1
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-07-04 22:12:19 +0100

    WIP: added javascript functionality for side menu on index page

--- components/sideMenu.js
+++ components/sideMenu.js
@@ -1,1 +1,28 @@
-console.log("test");
+// function to change active/displayed article & menu link
+function activateArticle(event) {
+  // loop through NodeList of articles, removing css that displays them
+  articles.forEach(article => article.classList.remove("article-active"));
+  // grab the data-tab of selected article
+  activeArticle = document.querySelector(
+    `.article[data-tab="${this.dataset.tab}"`
+  );
+  // update css to display article of corresponding data-tab
+  activeArticle.classList.add("article-active");
+
+  // loop through NodeList of menu links, removing 'active' css styles
+  menuLinks.forEach(link => link.classList.remove("menu-active"));
+  // grab the data-tab of selected menu link
+  activeMenuLink = document.querySelector(
+    `.article-link[data-tab="${this.dataset.tab}"`
+  );
+  // update css styles of active menu link
+  activeMenuLink.classList.add("menu-active");
+}
+
+// grab all article links from menu
+let menuLinks = document.querySelectorAll(".article-link");
+// grab all articles
+let articles = document.querySelectorAll(".article");
+
+// loop through left side menu to add click event listeners to links, to invoke activate article function
+menuLinks.forEach(link => link.addEventListener("click", activateArticle));
